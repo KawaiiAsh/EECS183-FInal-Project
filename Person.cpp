fb90b347226b88e142a658123a453d52d277fab1
KaibutsuSama <xinleroy@umich.edu>
2024-04-01 22:31:44 -0400

    update: person.cpp

--- Person.cpp
+++ Person.cpp
@@ -18,18 +18,49 @@
 using namespace std;
 
 Person::Person(string inputString) : Person() {
-    //TODO: Implement non-default constructor
+    if (inputString.size() >= 8) {
+        if (inputString[1] != 'f') {
+            int i = inputString[0] - '0';
+            int j = inputString[1] - '0';
+            turn = 10 * i + j;
+            currentFloor = inputString[3] - '0';
+            targetFloor = inputString[5] - '0';
+            angerLevel = inputString[7] - '0';
+        } else {
+            stringstream ss(inputString);
+            ss >> turn;
+            char floorLetter;
+            ss >> floorLetter;
+            ss >> currentFloor;
+            char target;
+            ss >> target;
+            ss >> targetFloor;
+            char anger;
+            ss >> anger;
+            ss >> angerLevel;
+        }
+    } else {
+        // Handle invalid input here, such as throwing an exception or setting default values.
+        // For simplicity, let's assume setting default values.
+        turn = 0;
+        currentFloor = 0;
+        targetFloor = 0;
+        angerLevel = 0;
+    }
 }
 
 bool Person::tick(int currentTime) {
-    //TODO: Implement tick
-
-    //Returning false to prevent compilation error
-    return false;
+    if (currentTime % TICKS_PER_ANGER_INCREASE == 0){
+        angerLevel++;
+    } if (angerLevel >= MAX_ANGER){
+        return true;
+    } else{
+        return false;
+    }
 }
 
-void Person::print(ostream &outs) {    
-    //TODO: Implement print
+void Person::print(ostream &outs) {
+    outs << "f" << currentFloor << "t" << targetFloor << "a" << angerLevel << endl;
 }
 
 //////////////////////////////////////////////////////
@@ -47,20 +78,19 @@ int Person::getTurn() const {
     return turn;
 }
 
-int Person::getCurrentFloor() const {    
+int Person::getCurrentFloor() const {
     return currentFloor;
 }
 
-int Person::getTargetFloor() const {    
+int Person::getTargetFloor() const {
     return targetFloor;
 }
 
-int Person::getAngerLevel() const {    
+int Person::getAngerLevel() const {
     return angerLevel;
 }
 
-ostream& operator<< (ostream& outs, Person p)
-{
+ostream &operator<<(ostream &outs, Person p) {
     p.print(outs);
     return outs;
 }
